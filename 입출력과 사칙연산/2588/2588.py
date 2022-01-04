0ca9066a6345e1caa69a137dd5f02daf67843233
Youngcheor Ra <dksrudmoe@naver.com>
2022-01-04 16:42:05 +0900

    Update 2588.py

--- 입출력과 사칙연산/2588/2588.py
+++ 입출력과 사칙연산/2588/2588.py
@@ -1,2 +1,3 @@
-a, b = map(int, input().split())
-print(b % 10 * a, b / 10 % 10 * a, b / 100 * a, a * b, sep="\n")
+a = int(input())
+b = int(input())
+print(b % 10 * a, b // 10 % 10 * a, b // 100 * a, a * b, sep="\n")
