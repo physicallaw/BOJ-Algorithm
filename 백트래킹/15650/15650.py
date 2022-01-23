82f6fa3a3c6c72ac824f3961b58f7d572023e26c
Youngcheor Ra <dksrudmoe@naver.com>
2022-01-23 19:10:36 +0900

    Create 15650

---
+++ 백트래킹/15650/15650.py
@@ -0,0 +1,15 @@
+def nandm(arr, n, m):
+   if m == 0:
+      for i in arr:
+         print(i, end=' ')
+      print()
+      return
+   for i in range(n):
+      if arr and arr[-1] >= i + 1:
+         continue
+      temp = arr.copy()
+      temp.append(i + 1)
+      nandm(temp, n, m - 1)
+  
+n, m = map(int, input().split())
+nandm([], n, m)
