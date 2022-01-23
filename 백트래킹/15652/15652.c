f50c2fdde47a43d22874a49ee64da8a249810adc
Youngcheor Ra <dksrudmoe@naver.com>
2022-01-24 07:46:54 +0900

    Create 15652

---
+++ 백트래킹/15652/15652.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#define MAX 9
+
+void nandm(int*, int, int);
+int find(int*, int, int);
+
+int main()
+{
+   int n, m;
+   int arr[MAX] = {0};
+   scanf("%d %d", &n, &m);
+   nandm(arr, n, m);
+
+   return 0;
+}
+
+int find(int* arr, int n, int val)
+{
+   int i;
+   for (i = 0; i < n; ++i)
+      if (arr[i] == val)
+         return i;
+   return -1;
+}
+
+void nandm(int* arr, int n, int m)
+{
+   int i, j;
+   int temp[MAX];
+   if (m == 0)
+   {
+      for (i = 0; arr[i] != 0; ++i)
+         printf("%d ", arr[i]);
+      printf("\n");
+      return;
+   }
+   for (i = 0; i < n; ++i)
+   {
+      if (arr[0] != 0 && arr[find(arr, n, 0) - 1] > i + 1) continue;
+      for (j = 0; j < MAX; ++j)
+         temp[j] = arr[j];
+      for (j = 0; j < n; ++j)
+         if (temp[j] == 0)
+         {
+            temp[j] = i + 1;
+            break;
+         }
+      nandm(temp, n, m - 1);
+   }
+}
---
+++ 백트래킹/15652/Main.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+	public static StringBuilder sb = new StringBuilder();
+	public static void nandm(ArrayList<Integer> arr, int n, int m) {
+		if (m == 0)
+		{
+			for (int i = 0; i < arr.size(); ++i)
+				sb.append(Integer.toString(arr.get(i)) + " ");
+			sb.append("\n");
+			return;
+		}
+		for (int i = 0; i < n; ++i)
+		{
+			if (!arr.isEmpty() && arr.get(arr.size() - 1) > i + 1) continue;
+			ArrayList<Integer> temp = (ArrayList<Integer>) arr.clone();
+			temp.add(i + 1);
+			nandm(temp, n, m - 1);
+		}		
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		nandm(arr, n, m);
+		System.out.print(sb);
+		sc.close();
+	}
+}
