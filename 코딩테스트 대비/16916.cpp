fe56007befbfc9fcdb3d2f987deda4eacfbc25b1
Youngcheor Ra <dksrudmoe@naver.com>
2022-03-18 22:01:02 +0900

    Create 16916.cpp

---
+++ 코딩테스트 대비/16916.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> makeTable(string pattern)
+{
+   int patternSize = pattern.size();
+   vector<int> table(patternSize, 0);
+   int j = 0;
+   for (int i = 1; i < patternSize; i++)
+   {
+      while (j > 0 && pattern[i] != pattern[j])
+         j = table[j - 1];
+      if (pattern[i] == pattern[j])
+         table[i] = ++j;
+   }
+   return table;
+}
+
+int KMP(string parent, string pattern)
+{
+   vector<int> table = makeTable(pattern);
+   int parentSize = parent.size();
+   int patternSize = pattern.size();
+   int j = 0;
+   for (int i = 0; i < parentSize; i++)
+   {
+      while (j > 0 && parent[i] != pattern[j])
+         j = table[j - 1];
+      if (parent[i] == pattern[j])
+         if (j == patternSize - 1)
+            return 1;
+         else
+            j++;
+   }
+   return 0;
+}
+
+int main(void)
+{
+   string a, b;
+   cin >> a >> b;
+   cout << KMP(a, b);
+   return 0;
+}
