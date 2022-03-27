6f02cc426a28d2e36e0a4bf8bffcc0b442c20716
Youngcheor Ra <dksrudmoe@naver.com>
2022-03-27 19:57:31 +0900

    Create 11403.cpp

---
+++ 그래프/11403/11403.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<vector<int>> v(n, vector<int>(n));
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j)
+            cin >> v[i][j];
+    for (int l = 0; l < 2; ++l)
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < n; ++j)
+                if (!v[i][j])
+                    for (int k = 0; k < n; ++k)
+                        if (v[i][k] and v[k][j])
+                            v[i][j] = 1;
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+            cout << v[i][j] << " ";
+        cout << "\n";
+    }
+
+    return 0;
+}
