a97fa7836e97de219d6bd0f3f15c55eb9c178823
Youngcheor Ra <dksrudmoe@naver.com>
2022-04-15 08:36:17 +0900

    Update 2609.py

--- 코딩테스트 대비/2609.py
+++ 코딩테스트 대비/2609.py
@@ -1,8 +1,8 @@
-n, m = map(int, input().split())
-
 def gcd(a, b):
     if b:
         return gcd(b, a % b)
     return a
 
+n, m = map(int, input().split())
 print(gcd(n, m))
+print(n * m // gcd(n, m))
